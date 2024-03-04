7559424b425d5b13f277dae7f573b5dc863b41fd
Limanov <kamilkalota@gmail.com>
2024-03-04 19:23:54 +0100

    Add canvas elements

---
+++ Js/arkanoid.js
@@ -0,0 +1,6 @@
+var cvGame = document.getElementById('cvGame').getContext('2d');
+
+cvGame.fillStyle = 'black';
+cvGame.fillRect(10, 10, 400, 400);
+cvGame.fillStyle = 'green';
+cvGame.fillRect(150, 380, 100, 15);
