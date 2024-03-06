2766ea333db6d801e98e93db93d3a7acd8944557
Limanov <kamilkalota@gmail.com>
2024-03-06 20:21:00 +0100

    Paddle Move

--- Js/arkanoid.js
+++ Js/arkanoid.js
@@ -13,6 +13,14 @@ start : function(){
     this.context = this.canvas.getContext('2d');
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.interval = setInterval(updateAGame, 20);
+    window.addEventListener('keydown', function (e) {
+      e.preventDefault();
+      aGame.keys = (aGame.keys || []);
+      aGame.keys[e.keyCode] = (e.type == "keydown");
+  })
+  window.addEventListener('keyup', function (e) {
+    aGame.keys[e.keyCode] = (e.type == "keydown");
+  })
 },
 clear : function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -22,6 +30,7 @@ clear : function() {
 function component(width, height, color, x, y) {
         this.width = width;
         this.height = height;
+        this.speed = 0;
         this.x = x;
         this.y = y;
         this.update = function () {
@@ -29,10 +38,16 @@ function component(width, height, color, x, y) {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
+        
+        this.newPosition = function() {
+        this.x += this.speed;
+    }
     }
 
   function updateAGame() {
     aGame.clear();
-    paddle.x += 1;
+    if (aGame.keys && aGame.keys[39]) {paddle.speed= 3;}
+    if (aGame.keys && aGame.keys[37]) {paddle.speed= -3;}
+    paddle.newPosition();
     paddle.update();
   }
