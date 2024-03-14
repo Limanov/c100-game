c06c5943591b8cc47bc65e70cd9bbe6a41cb08d7
Limanov <kamilkalota@gmail.com>
2024-03-14 23:04:03 +0100

    Change ball movement

--- Js/arkanoid.js
+++ Js/arkanoid.js
@@ -53,7 +53,7 @@ function component(width, height, color, x, y) {
       this.x = x;
       this.y = y;
       this.goUp = true;
-      this.goUp = true;
+      this.isTestBall = false;
       this.update = function () {
           ctx = aGame.context;
           ctx.beginPath();
@@ -68,12 +68,30 @@ function component(width, height, color, x, y) {
       {
         this.x += this.speed;
       }
-      else {
-      if(this.goUp)
+      else 
+      {
+      if(this.goUp){
       this.y += this.speed;
-      else
+      }
+      else{
       this.y -= this.speed;
-      }  
+      }
+      }
+      if(this.isTestBall){
+      if(this.goUp) 
+      this.x -= this.speed;
+       else
+       this.x += this.speed;
+       if(this.x >= 640  && this.speed == -3)
+       {  
+
+         this.speed = 3; 
+       }
+      //  else if (this.x >= 376 && this.speed)  
+      //  {
+      //    this.goUp = testBall.speed;
+      //  }
+      }
       if(this.y <= 1)
       {  
         this.goUp = false; 
@@ -82,13 +100,14 @@ function component(width, height, color, x, y) {
       {
         this.goUp = true;
       }
+    }
   }
-  }
+
   function updateAGame() {
     aGame.clear();
     if (aGame.keys && aGame.keys[39]) {paddle.speed = 3; if(!ball.start){ball.speed = 3; testBall.speed = 3;}}
     if (aGame.keys && aGame.keys[37]) {paddle.speed = -3; if(!ball.start){ball.speed = -3; testBall.speed = -3;}}
-    if (aGame.keys && aGame.keys[32]) {ball.speed = -3; ball.start = true; testBall.speed = -3; testBall.start = true;}
+    if (aGame.keys && aGame.keys[32]) {ball.speed = -3; ball.start = true; testBall.speed = -3; testBall.start = true;testBall.isTestBall = true;}
 
     paddle.newPosition();
     paddle.update();
