d7a01f7d4b7a66c1b19ae7bcad94a6292a5f7083
Limanov <kamilkalota@gmail.com>
2024-05-19 22:30:15 +0200

    Add brick

--- Js/arkanoid.js
+++ Js/arkanoid.js
@@ -29,6 +29,7 @@ function oneBrick(){
   bricks[0][3] = { x: 0, y: 0 , IsVisible: 1, hits: 1};
   bricks[1][0] = { x: 0, y: 0 , IsVisible: 1, hits: 1};
   bricks[1][1] = { x: 0, y: 0 , IsVisible: 1, hits: 1};
+  bricks[1][2] = { x: 0, y: 0 , IsVisible: 1, hits: 1};
 }
 if(!isCustomTest){
   newBricks();
