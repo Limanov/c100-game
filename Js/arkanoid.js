110297540f81571bed762bed441bbf69a87a60b5
Limanov <kamilkalota@gmail.com>
2024-06-08 23:57:24 +0200

    Add comment

--- Js/arkanoid.js
+++ Js/arkanoid.js
@@ -50,7 +50,7 @@ function oneBrick(){
   bricks[3][3] = { x: 0, y: 0 , IsVisible: 1, hits: 1};
   bricks[3][4] = { x: 0, y: 0 , IsVisible: 0, hits: 1};
 }
-if (stage === -2){
+if (stage === -2){//new stage
   brickRowCount = 5;
   brickColumnCount = 4;
   bricks[0] = []; //test
